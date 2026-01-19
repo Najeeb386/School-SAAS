a1c22b08d66604d371e80a373f088ef163baa06d
Najeeb386 <najeebhassan386@gmail.com>
2026-01-20 00:03:13 +0500

    Login,register amd index page UI updated for SAAS admin

---
+++ App/Modules/SAAS_admin/include/sidebar.php
@@ -0,0 +1,30 @@
+<aside class="app-navbar">
+                    <!-- begin sidebar-nav -->
+                    <div class="sidebar-nav scrollbar scroll_light">
+                        <ul class="metismenu " id="sidebarNav">
+                            <li class="nav-static-title">Personal</li>
+                            <li class="active">
+                                <a class="has-arrow" href="javascript:void(0)" aria-expanded="false">
+                                    <i class="nav-icon ti ti-rocket"></i>
+                                    <span class="nav-title">Dashboards</span>
+                                    <span class="nav-label label label-danger">9</span>
+                                </a>
+                                <ul aria-expanded="false">
+                                    <li class="active"> <a href='index.html'>Default</a> </li>
+                                    <li> <a href='index-ecommerce.html'>Ecommerce</a> </li>
+                                    <li> <a href='index-car-dealer.html'>Car Dealer</a> </li>
+                                    <li> <a href='index-stock-market.html'>Stock Market</a> </li>
+                                    <li> <a href='index-dating.html'>Dating</a> </li>
+                                    <li> <a href='index-job-portal.html'>Job Portal</a> </li>
+                                    <li> <a href='index-crm.html'>CRM</a> </li>
+                                    <li> <a href='index-real-estate.html'>Real Estate</a> </li>
+                                    <li> <a href='index-crypto-currency.html'>Crypto Currency</a> </li>
+                                </ul>
+                            </li>
+                            <li><a href="app-chat.html" aria-expanded="false"><i class="nav-icon ti ti-comment"></i><span class="nav-title">Schools</span></a> </li>
+                            
+
+                        </ul>
+                    </div>
+                    <!-- end sidebar-nav -->
+                </aside>
