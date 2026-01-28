a49eddd046a5318150df25a7e6e9fcba8d005b56
Najeeb386 <najeebhassan386@gmail.com>
2026-01-28 11:20:52 +0500

    start work on school admin dashboard and found a bug like it some time redirect from login to saas_admin when we do back here

--- App/Modules/School_Admin/include/navbar.php
+++ App/Modules/School_Admin/include/navbar.php
@@ -7,6 +7,7 @@
                             <img src="../../../../../public/assets/img/logo.png" class="img-fluid logo-desktop" alt="logo" />
                             <img src="../../../../../public/assets/img/logo-icon.png" class="img-fluid logo-mobile" alt="logo" />
                         </a>
+                      
                     </div>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <i class="ti ti-align-left"></i>
@@ -31,124 +32,8 @@
                             </ul>
                             <ul class="navbar-nav nav-right ml-auto">
                                
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="javascript:void(0)" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fe fe-bell"></i>
-                                        <span class="notify">
-                                                    <span class="blink"></span>
-                                        <span class="dot"></span>
-                                        </span>
-                                    </a>
-                                    <div class="dropdown-menu extended animated fadeIn" aria-labelledby="navbarDropdown">
-                                        <ul>
-                                            <li class="dropdown-header bg-gradient p-4 text-white text-left">Notifications
-                                                <a href="#" class="float-right btn btn-square btn-inverse-light btn-xs m-0">
-                                                    <span class="font-13"> Clear all</span></a>
-                                            </li>
-                                            <li class="dropdown-body min-h-240 nicescroll">
-                                                <ul class="scrollbar scroll_dark max-h-240">
-                                                    <li>
-                                                        <a href="javascript:void(0)">
-                                                            <div class="notification d-flex flex-row align-items-center">
-                                                                <div class="notify-icon bg-img align-self-center">
-                                                                    <div class="bg-type bg-type-md">
-                                                                        <span>HY</span>
-                                                                    </div>
-                                                                </div>
-                                                                <div class="notify-message">
-                                                                    <p class="font-weight-bold">New registered user</p>
-                                                                    <small>Just now</small>
-                                                                </div>
-                                                            </div>
-                                                        </a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0)">
-                                                            <div class="notification d-flex flex-row align-items-center">
-                                                                <div class="notify-icon bg-img align-self-center">
-                                                                    <div class="bg-type bg-type-md bg-success">
-                                                                        <span>GM</span>
-                                                                    </div>
-                                                                </div>
-                                                                <div class="notify-message">
-                                                                    <p class="font-weight-bold">New invoice received</p>
-                                                                    <small>22 min</small>
-                                                                </div>
-                                                            </div>
-                                                        </a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0)">
-                                                            <div class="notification d-flex flex-row align-items-center">
-                                                                <div class="notify-icon bg-img align-self-center">
-                                                                    <div class="bg-type bg-type-md bg-danger">
-                                                                        <span>FR</span>
-                                                                    </div>
-                                                                </div>
-                                                                <div class="notify-message">
-                                                                    <p class="font-weight-bold">Server error report</p>
-                                                                    <small>7 min</small>
-                                                                </div>
-                                                            </div>
-                                                        </a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0)">
-                                                            <div class="notification d-flex flex-row align-items-center">
-                                                                <div class="notify-icon bg-img align-self-center">
-                                                                    <div class="bg-type bg-type-md bg-info">
-                                                                        <span>HT</span>
-                                                                    </div>
-                                                                </div>
-                                                                <div class="notify-message">
-                                                                    <p class="font-weight-bold">Database report</p>
-                                                                    <small>1 day</small>
-                                                                </div>
-                                                            </div>
-                                                        </a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0)">
-                                                            <div class="notification d-flex flex-row align-items-center">
-                                                                <div class="notify-icon bg-img align-self-center">
-                                                                    <div class="bg-type bg-type-md">
-                                                                        <span>DE</span>
-                                                                    </div>
-                                                                </div>
-                                                                <div class="notify-message">
-                                                                    <p class="font-weight-bold">Order confirmation</p>
-                                                                    <small>2 day</small>
-                                                                </div>
-                                                            </div>
-                                                        </a>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                            <li class="dropdown-footer">
-                                                <a class="font-13" href="javascript:void(0)"> View All Notifications
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link search" href="javascript:void(0)">
-                                        <i class="ti ti-search"></i>
-                                    </a>
-                                    <div class="search-wrapper">
-                                        <div class="close-btn">
-                                            <i class="ti ti-close"></i>
-                                        </div>
-                                        <div class="search-content">
-                                            <form>
-                                                <div class="form-group">
-                                                    <i class="ti ti-search magnifier"></i>
-                                                    <input type="text" class="form-control autocomplete" placeholder="Search Here" id="autocomplete-ajax" autofocus="autofocus">
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </li>
+                               
+                                
                                 <li class="nav-item dropdown user-profile">
                                     <a href="javascript:void(0)" class="nav-link dropdown-toggle " id="navbarDropdown4" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         <img src="../../../../../public/assets/img/avtar/02.jpg" alt="avtar-img">
