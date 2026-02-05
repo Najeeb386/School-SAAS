89e6bac0eb7fecb0081f9b8129538b32a1bc4876
Najeeb386 <najeebhassan386@gmail.com>
2026-02-05 21:32:39 +0500

    sidebar polished

--- App/Modules/School_Admin/include/sidebar.php
+++ App/Modules/School_Admin/include/sidebar.php
@@ -2,27 +2,88 @@
                     <!-- begin sidebar-nav -->
                     <div class="sidebar-nav scrollbar scroll_light">
                         <ul class="metismenu " id="sidebarNav">
-                            <li class="nav-static-title">Personal</li>
+                          <ul>
+
                             <li class="active">
                                 <a href="../../views/dashboard/index.php" aria-expanded="false">
-                                    <i class="nav-icon ti ti-rocket"></i>
-                                    <span class="nav-title">Dashboards</span>
-
+                                    <i class="nav-icon ti ti-home"></i>
+                                    <span class="nav-title">Dashboard</span>
                                 </a>
-                                
                             </li>
-                            
-                            <li><a href="../../views/students/students.php" aria-expanded="false"><i class="nav-icon ti ti-comment"></i><span class="nav-title">Student</span></a> </li>
-                            <li><a href="../../views/staff/staff.php" aria-expanded="false"><i class="nav-icon ti ti-comment"></i><span class="nav-title">Teachers</span></a> </li>
-                            <li><a href="../../views/classes/classes.php" aria-expanded="false"><i class="nav-icon ti ti-comment"></i><span class="nav-title">Classes</span></a> </li>
-                            <li><a href="../../views/attendence/attendence.php" aria-expanded="false"><i class="nav-icon ti ti-comment"></i><span class="nav-title">Attendence</span></a> </li>
-                            <li><a href="../../views/finance/finance.php" aria-expanded="false"><i class="nav-icon ti ti-comment"></i><span class="nav-title">Finance</span></a> </li>
-                            <li><a href="../../views/fees/fees.php" aria-expanded="false"><i class="nav-icon ti ti-comment"></i><span class="nav-title">Fees</span></a> </li>
-                            <li><a href="../../views/settings/setting.php" aria-expanded="false"><i class="nav-icon ti ti-comment"></i><span class="nav-title">Reports</span></a> </li>
-                            <li><a href="../../views/examination/examination.php" aria-expanded="false"><i class="nav-icon ti ti-comment"></i><span class="nav-title">Examination</span></a> </li>
-                            <li><a href="../../views/announcment/announcment.php" aria-expanded="false"><i class="nav-icon ti ti-comment"></i><span class="nav-title">Announcements</span></a> </li>
-                            <li><a href="../../views/settings/setting.php" aria-expanded="false"><i class="nav-icon ti ti-comment"></i><span class="nav-title">Settings</span></a> </li>
+
+                            <li>
+                                <a href="../../views/students/students.php" aria-expanded="false">
+                                    <i class="nav-icon ti ti-user"></i>
+                                    <span class="nav-title">Students</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="../../views/staff/staff.php" aria-expanded="false">
+                                    <i class="nav-icon ti ti-user"></i>
+                                    <span class="nav-title">Teachers</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="../../views/classes/classes.php" aria-expanded="false">
+                                    <i class="nav-icon ti ti-book"></i>
+                                    <span class="nav-title">Classes</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="../../views/attendence/attendence.php" aria-expanded="false">
+                                    <i class="nav-icon ti ti-calendar"></i>
+                                    <span class="nav-title">Attendance</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="../../views/finance/finance.php" aria-expanded="false">
+                                    <i class="nav-icon ti ti-credit-card"></i>
+                                    <span class="nav-title">Finance</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="../../views/fees/fees.php" aria-expanded="false">
+                                    <i class="nav-icon ti ti-money"></i>
+                                    <span class="nav-title">Fees</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="../../views/settings/setting.php" aria-expanded="false">
+                                    <i class="nav-icon ti ti-credit-card"></i>
+                                    <span class="nav-title">Reports</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="../../views/examination/examination.php" aria-expanded="false">
+                                    <i class="nav-icon ti ti-pencil"></i>
+                                    <span class="nav-title">Examination</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="../../views/announcment/announcment.php" aria-expanded="false">
+                                    <i class="nav-icon ti ti-bell"></i>
+                                    <span class="nav-title">Announcements</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="../../views/settings/setting.php" aria-expanded="false">
+                                    <i class="nav-icon ti ti-settings"></i>
+                                    <span class="nav-title">Settings</span>
+                                </a>
+                            </li>
+
+                            </ul>
                         </ul>
+
                     </div>
                     <!-- end sidebar-nav -->
                 </aside>
